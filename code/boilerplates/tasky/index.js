102e4add1e104495e25564b5ca7d87d3cf4f4982
Anthony Campbell <anthony@anthonyjcampbell.com>
2020-04-16 16:55:19 +0100

    Pushing old work

--- code/boilerplates/tasky/index.js
+++ code/boilerplates/tasky/index.js
@@ -22,13 +22,24 @@ app.on("ready", () => {
   const iconName =
     process.platform === "win32" ? "windows-icon.png" : "iconTemplate.png";
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+
   tray = new Tray(iconPath);
-  tray.on("click", () => {
+
+  tray.on("click", (e, bounds) => {
+    const { x, y } = bounds;
+    const { height, width } = mainWindow.getBounds();
+
     if (mainWindow.isVisible()) {
       mainWindow.hide();
     } else {
+      const yPosition = process.platform === "darwin" ? y : y - height;
+      mainWindow.setBounds({
+          x: x - width / 2,
+          y: yPosition,
+          height,
+          width,
+      });
       mainWindow.show();
     }
   });
 });
- 
